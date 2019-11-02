c0e4a92434c27e457081d1f07f49a37eccfea0fd
0401morita <umacha_umai@yahoo.co.jp>
2019-11-03 03:56:04 +0900

    Deploy gh-pages

--- out/_next/static/chunks/styles.js
+++ out/_next/static/chunks/styles.js
@@ -103,7 +103,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1572720610307");
+            injectCss(link, link.href.split("?")[0] + "?unix=1572720953057");
           });
       }
     }
